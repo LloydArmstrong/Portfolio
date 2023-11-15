9a48bf5f60746f9ef08ef0040a97425befdd30a9
Lloyd Armstrong <lloyd.armstrong@metro-markets.de>
2023-11-15 16:42:37 +0100

    Initial Changes

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -135,30 +135,11 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
-            </p>
-
-            <p>
-              Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
-            </p>
-
-            <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
+              Hello! My name is Lloyd and I enjoy building and modernizing things. I am a GCP and
+              AWS Certified Engineer with 9 years experience in architecting, maintaining and
+              growing cloud-based platforms and complex software development lifecycles. A
+              musician-turned-tech aficionado, with a passion for automating things and new
+              technologies that make our lives easier. about HTML &amp; CSS!
             </p>
 
             <p>Here are a few technologies I’ve been working with recently:</p>
