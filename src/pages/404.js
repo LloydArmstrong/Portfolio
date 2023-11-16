06d3503aac1d2ceb57c13da265688d4d26555984
Lloyd Armstrong <lloyd.armstrong@metro-markets.de>
2023-11-16 16:31:47 +0100

    404 Changes

--- src/pages/404.js
+++ src/pages/404.js
@@ -43,7 +43,7 @@ const NotFoundPage = ({ location }) => {
   const content = (
     <StyledMainContainer className="fillHeight">
       <StyledTitle>404</StyledTitle>
-      <StyledSubtitle>Page Not Found</StyledSubtitle>
+      <StyledSubtitle>What are you looking for, huh?</StyledSubtitle>
       <StyledHomeButton to="/">Go Home</StyledHomeButton>
     </StyledMainContainer>
   );
