0d27f9397686cfe6a9aed6702f825784952c3cee
Lloyd Armstrong <lloyd.armstrong@metro-markets.de>
2023-11-17 13:30:03 +0100

    SVG Rework

--- src/components/icons/loader.js
+++ src/components/icons/loader.js
@@ -5,7 +5,10 @@ const IconLoader = () => (
     <title>Loader Logo</title>
     <g>
       <g id="LA" transform="translate(11.000000, 5.000000)">
-        <path d="M46,61 L29,61 L29,31 L33,31 L33,57 L46,57 Z Z Z" fill="currentColor" />
+        <path
+          d="M6496 6358 c-3 -18 -15 -96 -27 -173 -11 -77 -90 -432 -175 -789 -85 -357 -154 -653 -154 -658 0 -4 167 -8 370 -8 204 0 370 4 370 9 0 4 -65 282 -144 617 -146 616 -189 815 -201 934 -3 36 -9 73 -11 83 -8 28 -23 20 -28 -15z"
+          fill="currentColor"
+        />
       </g>
       <path
         stroke="currentColor"
